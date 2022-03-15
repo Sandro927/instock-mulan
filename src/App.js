83a4c7b3a58595ba1c1636d95fdf5c9ee8883f5d
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-15 18:24:32 -0400

    Setup all basic routes including dynamic warehouse and dynamic inventory routes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
     return (
         <BrowserRouter>
             <Header />
-
+            <Switch>
+                {/* <Route path='/' exact component={WarehouseList} />
+                <Route path='/warehouse/:warehouseId' component={WarehouseDetails} />
+                <Route path='/inventory' exact component={InventoryList} />
+                <Route path='/inventory/:inventoryId' component={InventoryItemDetails} /> */}
+            </Switch>
             <Footer />
         </BrowserRouter>
     );
