50462dc5e245cfe5d9052954d5c7ef9cb65c88a6
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-18 16:21:11 -0400

    fixed app.js conflict

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.scss';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import Warehouses from './pages/Warehouses';
+import Warehouses from './pages/Warehouses/Warehouses';
 import Inventory from './pages/Inventory/Inventory';
 import AddItem from './pages/AddItem/AddItem';
-// import InventoryItemDetails from './Components/InventoryItemDetails/InventoryItemDetails';
+import InventoryItemDetails from './Components/InventoryItemDetails/InventoryItemDetails';
 
 function App() {
 
