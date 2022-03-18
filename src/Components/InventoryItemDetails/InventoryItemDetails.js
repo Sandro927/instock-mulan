900d962d78b5169b7be7ca2289d902931b988dac
Charanpreet Chall <charanpreetchall@gmail.com>
2022-03-18 15:16:22 -0400

    Completed the functionality and the responsive design for Inventory Item Component

--- src/Components/InventoryItemDetails/InventoryItemDetails.js
+++ src/Components/InventoryItemDetails/InventoryItemDetails.js
@@ -2,58 +2,82 @@ import './InventoryItemDetails.scss'
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/icons/arrow_back-24px.svg';
 import edit from '../../assets/icons/edit-24px.svg';
+import React from 'react';
+import axios from 'axios';
+
+class InventoryItemDetails extends React.Component {
+
+    state = {
+        inventoryItem: {}
+    }
+
+    componentDidMount() {
+        const {inventoryId} = this.props.match.params
+        axios
+            .get(`http://localhost:8080/inventories/${inventoryId}`)
+            .then(res => {
+                this.setState({ inventoryItem: res.data })
+            })
+            .catch(err => console.error)
+    }
 
 
-import React from 'react'
+    render() {
+        const { inventoryItem } = this.state;
 
-function InventoryItemDetails() {
-  return (
+        const text = inventoryItem.status
 
-    <div className="item">
-        <div className="item__header">
-            <Link className="item__link" to="/inventories">
-                <img className="item__icon-arrow" src={arrow} alt="Back Arrow" />
-            </Link>
-            <h1 className="item__heading">Television</h1>
-            <Link className="item__link-edit" href="/inventories" >
-                <img className="item__icon-edit" src={edit} alt="Edit Item" />
-                <p className="item__icon-text">Edit</p>
-            </Link>
-        </div>
+        return (
 
-        <div className="item__underline"></div>
+            <div className="item">
+                <div className="item__header">
+                    <Link className="item__link" to="/inventories">
+                        <img className="item__icon-arrow" src={arrow} alt="Back Arrow" />
+                    </Link>
+                    <h1 className="item__heading">{inventoryItem.itemName}</h1>
+                    <Link className="item__link-edit" to="/inventories" >
+                        <img className="item__icon-edit" src={edit} alt="Edit Item" />
+                        <p className="item__icon-text">Edit</p>
+                    </Link>
+                </div>
 
-        <div className="item__details-container">
+                <div className="item__underline"></div>
 
-            <div className="item__details">
-                <h4 className="item__subheading">ITEM DESCRIPTION:</h4>
-                <p className="item__information">This 50", 4K LED TV provides a crystal-clear picture and vivid colors.</p>
+                <div className="item__details-container">
+                <div className="item__divider"></div>
+
+                    <div className="item__details">
+                        <h4 className="item__subheading">ITEM DESCRIPTION:</h4>
+                        <p className="item__information">{inventoryItem.itemName}</p>
+                    </div>
+
+                    <div className="item__details">
+                        <h4 className="item__subheading">CATEGORY:</h4>
+                        <p className="item__information">{inventoryItem.category}</p>
+                    </div>
+
+                    <div className="item__details-status">
+                        <h4 className="item__subheading">STATUS:</h4>
+                        <p className={text === "In Stock" ? "item__information--green" : 'item__information--red'} >
+                            {text}
+                        </p>
+                    </div>
+
+                    <div className="item__details-quantity">
+                        <h4 className="item__subheading">QUANTITY:</h4>
+                        <p className="item__information">{inventoryItem.quantity}</p>
+                    </div>
+
+                    <div className="item__details">
+                        <h4 className="item__subheading">WAREHOUSE:</h4>
+                        <p className="item__information">{inventoryItem.warehouseName}</p>
+                    </div>
+
+                </div>
+
             </div>
 
-            <div className="item__details">
-                <h4 className="item__subheading">CATEGORY:</h4>
-                <p className="item__information">Electronics</p>
-            </div>
-
-            <div className="item__details-status">
-                <h4 className="item__subheading">STATUS:</h4>
-                <p className="item__information--green">IN STOCK</p>
-            </div>
-
-            <div className="item__details-quantity">
-                <h4 className="item__subheading">QUANTITY:</h4>
-                <p className="item__information">500</p>
-            </div>
-
-            <div className="item__details">
-                <h4 className="item__subheading">WAREHOUSE:</h4>
-                <p className="item__information">Manhattan</p>
-            </div>
-            
-        </div>
-
-    </div>
-
-  )}
-
+        )
+    }
+}
 export default InventoryItemDetails;
