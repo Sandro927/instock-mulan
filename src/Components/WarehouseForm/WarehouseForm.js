c942a3039fd881cbe54aa29300dc0817e6f4af12
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-19 19:39:48 -0400

    Created html for entire WarehouseForm component

--- src/Components/WarehouseForm/WarehouseForm.js
+++ src/Components/WarehouseForm/WarehouseForm.js
@@ -12,17 +12,58 @@ class WarehouseForm extends React.Component {
         formData: null
     };
 
-    // componentDidUpdate() {
 
-    // }
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
 
     render() {
 
         return (
-            <section>
+            <form onSubmit={this.handleSubmit} className='warehouse-form'>
 
-            </section>
+                <section className='warehouse-form__section-container'>
+                    <h2 className='form-detail__title'>Warehouse Details</h2>
+
+                    <label className='form-detail__label' htmlFor='warehouseName'>Warehouse Name</label>
+                    <input className='form-detail__input' name='warehouseName' type="text" placeholder='Warehouse Name' />
+
+                    <label className='form-detail__label' htmlFor='address'>Street Address</label>
+                    <input className='form-detail__input' name='address' type="text" placeholder='Street Address' />
+
+                    <label className='form-detail__label' htmlFor='city'>City</label>
+                    <input className='form-detail__input' name='city' type="text" placeholder='City' />
+
+                    <label className='form-detail__label' htmlFor='country'>Country</label>
+                    <input className='form-detail__input' name='country' type="text" placeholder='Country' />
+                </section>
+
+                <section className='warehouse-form__section-container'>
+                    <h2 className='form-detail__title'>Contact Details</h2>
+
+                    <label className='form-detail__label' htmlFor='contactName'>Contact Name</label>
+                    <input className='form-detail__input' name='contactName' type="text" placeholder='Contact Name' />
+
+                    <label className='form-detail__label' htmlFor='position'>Position</label>
+                    <input className='form-detail__input' name='position' type="text" placeholder='Position' />
+
+                    <label className='form-detail__label' htmlFor='phone'>Phone Number</label>
+                    <input className='form-detail__input' name='phone' type="tel" placeholder='Phone Number' />
+                    {/*  */}
+
+                    <label className='form-detail__label' htmlFor='email'>Email</label>
+                    <input className='form-detail__input' name='email' type="email" placeholder='Email' />
+                    {/*  */}
+
+                </section>
+
+                <div className='warhouse-form__button-container'>
+                    <Link to='/warehouses' className='button--cancel'>Cancel</Link>
+                    <button className='button--submit'>Add</button>
+                </div>
+
+            </form>
         );
     }
 };
