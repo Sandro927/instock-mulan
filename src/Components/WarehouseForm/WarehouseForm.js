52481ce85536567d2c8bae006287e90248d8f21d
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-20 05:03:22 -0400

    Finished styling entire AddWarehouse page and WarehouseForm components and making it responsive at all breakpoints

--- src/Components/WarehouseForm/WarehouseForm.js
+++ src/Components/WarehouseForm/WarehouseForm.js
@@ -80,60 +80,65 @@ class WarehouseForm extends React.Component {
 
         return (
             <form onSubmit={this.handleSubmit} className='warehouse-form'>
+                <div className='warehouse-form__top-container'>
 
-                <section className='warehouse-form__section-container'>
-                    <h2 className='form-detail__title'>Warehouse Details</h2>
+                    <section className='warehouse-form__section-container'>
+                        <h2 className='form-detail__title'>Warehouse Details</h2>
 
-                    <label className='form-detail__label' htmlFor='warehouseName'>Warehouse Name</label>
-                    <input className='form-detail__input' name='warehouseName' type="text" placeholder='Warehouse Name'
-                        onChange={this.handleChange}
-                        value={this.state.warehouseName} />
+                        <label className='form-detail__label' htmlFor='warehouseName'>Warehouse Name</label>
+                        <input className='form-detail__input' name='warehouseName' type="text" placeholder='Warehouse Name'
+                            onChange={this.handleChange}
+                            value={this.state.warehouseName} />
 
-                    <label className='form-detail__label' htmlFor='address'>Street Address</label>
-                    <input className='form-detail__input' name='address' type="text" placeholder='Street Address'
-                        onChange={this.handleChange}
-                        value={this.state.address} />
+                        <label className='form-detail__label' htmlFor='address'>Street Address</label>
+                        <input className='form-detail__input' name='address' type="text" placeholder='Street Address'
+                            onChange={this.handleChange}
+                            value={this.state.address} />
 
-                    <label className='form-detail__label' htmlFor='city'>City</label>
-                    <input className='form-detail__input' name='city' type="text" placeholder='City'
-                        onChange={this.handleChange}
-                        value={this.state.city} />
+                        <label className='form-detail__label' htmlFor='city'>City</label>
+                        <input className='form-detail__input' name='city' type="text" placeholder='City'
+                            onChange={this.handleChange}
+                            value={this.state.city} />
 
-                    <label className='form-detail__label' htmlFor='country'>Country</label>
-                    <input className='form-detail__input' name='country' type="text" placeholder='Country'
-                        onChange={this.handleChange}
-                        value={this.state.country} />
-                </section>
+                        <label className='form-detail__label' htmlFor='country'>Country</label>
+                        <input className='form-detail__input' name='country' type="text" placeholder='Country'
+                            onChange={this.handleChange}
+                            value={this.state.country} />
+                    </section>
 
-                <section className='warehouse-form__section-container'>
-                    <h2 className='form-detail__title'>Contact Details</h2>
+                    <section className='warehouse-form__section-container'>
+                        <h2 className='form-detail__title'>Contact Details</h2>
 
-                    <label className='form-detail__label' htmlFor='contactName'>Contact Name</label>
-                    <input className='form-detail__input' name='contactName' type="text" placeholder='Contact Name'
-                        onChange={this.handleChange}
-                        value={this.state.contactName} />
+                        <label className='form-detail__label' htmlFor='contactName'>Contact Name</label>
+                        <input className='form-detail__input' name='contactName' type="text" placeholder='Contact Name'
+                            onChange={this.handleChange}
+                            value={this.state.contactName} />
 
-                    <label className='form-detail__label' htmlFor='position'>Position</label>
-                    <input className='form-detail__input' name='position' type="text" placeholder='Position'
-                        onChange={this.handleChange}
-                        value={this.state.position} />
+                        <label className='form-detail__label' htmlFor='position'>Position</label>
+                        <input className='form-detail__input' name='position' type="text" placeholder='Position'
+                            onChange={this.handleChange}
+                            value={this.state.position} />
 
-                    <label className='form-detail__label' htmlFor='phone'>Phone Number</label>
-                    <input className='form-detail__input' name='phone' type="text" placeholder='Phone Number'
-                        onChange={this.handleChange}
-                        value={this.state.phone} />
+                        <label className='form-detail__label' htmlFor='phone'>Phone Number</label>
+                        <input className='form-detail__input' name='phone' type="text" placeholder='Phone Number'
+                            onChange={this.handleChange}
+                            value={this.state.phone} />
 
-                    <label className='form-detail__label' htmlFor='email'>Email</label>
-                    <input className='form-detail__input' name='email' type="text" placeholder='Email'
-                        onChange={this.handleChange}
-                        value={this.state.email} />
-                </section>
+                        <label className='form-detail__label' htmlFor='email'>Email</label>
+                        <input className='form-detail__input' name='email' type="text" placeholder='Email'
+                            onChange={this.handleChange}
+                            value={this.state.email} />
+                    </section>
+                </div>
+
 
                 <div className='warhouse-form__button-container'>
-                    <Link to='/warehouses' className='button--cancel'>Cancel</Link>
-                    <button type="submit" className='button--submit' >+ Add Warehouse</button>
+                    <Link to='/warehouses'>
+                        <button disabled="disabled" className='form__button form__button--cancel'>Cancel</button>
+                    </Link>
+                    <button type="submit" className='form__button form__button--submit' >+ Add Warehouse</button>
                 </div>
-                
+
             </form>
         );
     }
--- src/pages/AddWarehouse/AddWarehouse.js
+++ src/pages/AddWarehouse/AddWarehouse.js
@@ -1,6 +1,7 @@
 import './AddWarehouse.scss';
 import { Link } from 'react-router-dom';
 import WarehouseForm from '../../Components/WarehouseForm/WarehouseForm';
+import backArrow from '../../assets/icons/arrow_back-24px.svg';
 
 
 const AddWarehouse = (routerProps) => {
@@ -8,16 +9,15 @@ const AddWarehouse = (routerProps) => {
     return (
         <main className='main'>
             <div className='main__container'>
-                <header className='header'>
+                <header className='header--form-page'>
 
                     <Link to='/warehouses'>
-                        {/* chevron here */}
+                        <img src={backArrow} alt="icon of back/left arrow" />
                     </Link>
-
                     <h1 className='header__title'>Add Warehouse</h1>
                 </header>
 
-                {<WarehouseForm routerProps={routerProps}/>}
+                {<WarehouseForm routerProps={routerProps} />}
 
             </div>
         </main>
