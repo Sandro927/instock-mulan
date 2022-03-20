a3f759e05e8dfe03726df47be7a6f588a0e68006
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-20 00:31:11 -0400

    Created and added onChange handleChange handler to all input fields that changes/controls state of input field values

--- src/Components/WarehouseForm/WarehouseForm.js
+++ src/Components/WarehouseForm/WarehouseForm.js
@@ -13,12 +13,10 @@ class WarehouseForm extends React.Component {
         address: '',
         city: '',
         country: '',
-        contact: {
-            name: '',
-            position: '',
-            phone: '',
-            email: '',
-        },
+        contact: '',
+        position: '',
+        phone: '',
+        email: '',
         // formIsValid: false,
     };
 
@@ -52,6 +50,13 @@ class WarehouseForm extends React.Component {
         console.log(newWarehouse) // remove this when done
     }
 
+    // handleChange function changes/controls state of stored form values
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
 
     render() {
 
@@ -62,35 +67,48 @@ class WarehouseForm extends React.Component {
                     <h2 className='form-detail__title'>Warehouse Details</h2>
 
                     <label className='form-detail__label' htmlFor='warehouseName'>Warehouse Name</label>
-                    <input className='form-detail__input' name='warehouseName' type="text" placeholder='Warehouse Name' />
+                    <input className='form-detail__input' name='warehouseName' type="text" placeholder='Warehouse Name'
+                        onChange={this.handleChange}
+                        value={this.state.name} />
 
                     <label className='form-detail__label' htmlFor='address'>Street Address</label>
-                    <input className='form-detail__input' name='address' type="text" placeholder='Street Address' />
+                    <input className='form-detail__input' name='address' type="text" placeholder='Street Address'
+                        onChange={this.handleChange}
+                        value={this.state.address} />
 
                     <label className='form-detail__label' htmlFor='city'>City</label>
-                    <input className='form-detail__input' name='city' type="text" placeholder='City' />
+                    <input className='form-detail__input' name='city' type="text" placeholder='City'
+                        onChange={this.handleChange}
+                        value={this.state.city} />
 
                     <label className='form-detail__label' htmlFor='country'>Country</label>
-                    <input className='form-detail__input' name='country' type="text" placeholder='Country' />
+                    <input className='form-detail__input' name='country' type="text" placeholder='Country'
+                        onChange={this.handleChange}
+                        value={this.state.country} />
                 </section>
 
                 <section className='warehouse-form__section-container'>
                     <h2 className='form-detail__title'>Contact Details</h2>
 
                     <label className='form-detail__label' htmlFor='contactName'>Contact Name</label>
-                    <input className='form-detail__input' name='contactName' type="text" placeholder='Contact Name' />
+                    <input className='form-detail__input' name='contactName' type="text" placeholder='Contact Name'
+                        onChange={this.handleChange}
+                        value={this.state.contact} />
 
                     <label className='form-detail__label' htmlFor='position'>Position</label>
-                    <input className='form-detail__input' name='position' type="text" placeholder='Position' />
+                    <input className='form-detail__input' name='position' type="text" placeholder='Position'
+                        onChange={this.handleChange}
+                        value={this.state.position} />
 
                     <label className='form-detail__label' htmlFor='phone'>Phone Number</label>
-                    <input className='form-detail__input' name='phone' type="tel" placeholder='Phone Number' />
-                    {/*  */}
+                    <input className='form-detail__input' name='phone' type="text" placeholder='Phone Number'
+                        onChange={this.handleChange}
+                        value={this.state.phone} />
 
                     <label className='form-detail__label' htmlFor='email'>Email</label>
-                    <input className='form-detail__input' name='email' type="email" placeholder='Email' />
-                    {/*  */}
-
+                    <input className='form-detail__input' name='email' type="text" placeholder='Email'
+                        onChange={this.handleChange}
+                        value={this.state.email} />
                 </section>
 
                 <div className='warhouse-form__button-container'>
